c9792dc7fcf1c34d8b37e3425bf7e223ab4dd3fb
Mark Hills <mhills@acm.org>
2018-10-30 10:41:03 -0400

    Added new CanvasObserver to support Observer pattern

---
+++ src/main/java/edu/ecu/cs/csci3030/surfaces/ICanvasObserver.java
@@ -0,0 +1,11 @@
+package edu.ecu.cs.csci3030.surfaces;
+
+/**
+ * General interface for {@link Canvas} observers.
+ *
+ * @author Mark Hills
+ * @version 1.0
+ */
+public interface ICanvasObserver {
+    void update();
+}
